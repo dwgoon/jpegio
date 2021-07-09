e1eb1670152759557aa28117352c8ca1f93072de
EldritchJS <jschlessman@gmail.com>
2021-07-09 12:34:14 -0400

    Made cython and numpy even more obnoxiously needed

--- setup.py
+++ setup.py
@@ -128,7 +128,7 @@ ext_modules = [
               extra_link_args=largs,
               language="c++")
 ]
-
+setup_requirements = ['cython>=0.29','nupy>=1.13']
 install_requirements = ['cython>=0.29']
 requirements = ['cython>=0.29',
                 'numpy>=1.13',]
